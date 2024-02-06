af565e857d741caccd3d133e23486fb2589cb4d5
terter2033 <159159750+terter2033@users.noreply.github.com>
2024-02-06 23:14:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 57, 0881020];  // Enter here AppIDs of the needed games
+var games = [881020];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
